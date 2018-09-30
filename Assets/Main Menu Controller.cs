e4a75cf6122abc5da5d860895e7faff4abf23b7a
Geoff Anders <geoffanders@gmail.com>
2018-09-30 07:57:01 -0700

    adding Nicos work

---
+++ Assets/Main Menu Controller.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class NewBehaviourScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	public void New Fractal Geometry()
+	{
+		SceneManger.LoadScene("fractal geometry");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
